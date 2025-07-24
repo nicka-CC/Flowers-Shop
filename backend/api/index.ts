3fd0091eb8afa52d45bde2f31e38e9b47f0a9d73
nikita <im.nicka.cc@gmail.com>
2025-07-24 22:33:20 +0300

    init server

---
+++ backend/api/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@vendia/serverless-express';
+import * as express from 'express';
+
+let cachedServer: any;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    await app.init();
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  return cachedServer;
+}
+
+export default async function handler(req: any, res: any) {
+  const server = await bootstrapServer();
+  return server(req, res);
+} 
