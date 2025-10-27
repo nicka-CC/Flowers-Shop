cb3505097b117c796d727e68b80500e275f56d7f
NikitaBerdashev <im.nicka.cc@gmail.com>
2025-10-27 13:30:57 +0300

    init app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,11 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: [
+        'http://localhost:3000',
+        'https://flowers-shop-1.vercel.app',
+        'http://localhost:5173'
+      ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     },});
